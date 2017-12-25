d1ab2ac2bd8b00669e0d586622eff8d5989267de
mishalinkites1 <misha@linkites.com>
2017-12-25 19:04:12 +0530

    updates

--- routes/meeting_management.js
+++ routes/meeting_management.js
@@ -14,7 +14,9 @@ const busboyBodyParser = require('busboy-body-parser');
 const fs = require('fs');
 var AWS = require('aws-sdk');
 var moment = require("moment");
-
+var htmlConvert = require('html-convert');
+var convert = htmlConvert();
+var pdf = require('html-pdf');
 
 //AWS
 const BucketName = 'telospdf';
@@ -175,8 +177,100 @@ router.post('/updatePolls', (req,res)=>{
 
 router.post('/generateProxyForms', (req,res) => {
 
-
-
+var html = '<!DOCTYPE html>'+
+'<html>'+
+'<head>'+
+' <title>form</title>'+
+' <link rel="stylesheet" href="style.css">'+
+'</head>'+
+'<body>'+
+'  <style>'+
+'  .form-width{'+
+'  width: 745px;'+
+'    margin: 0 auto;'+
+'}'+
+'.heading-left{'+
+'    font-size: 20px;'+
+'    font-weight: 600;'+
+'    width: 70%;'+
+'    display: inline-block;'+
+'}'+
+'.heading-right{'+
+'  float: right;'+
+'    font-size: 20px;'+
+'    font-weight: 600;'+
+'    width: 15%;'+
+'}'+
+'.heading-instrument{'+
+'  text-align: center;'+
+'  margin: 31px 0 63px 0;'+
+'    font-weight: 100;'+
+'}'+
+'.form-contain{'+
+'  max-width: 650px;'+
+'  margin: 0 auto;'+
+'}'+
+'.form-contain p{'+
+'  font-size: 17px;'+
+'  line-height: 2;'+
+'}'+
+'.text-color{'+
+'  color: #CF5255;'+
+'}'+
+'.space{'+
+' text-indent: 50px;'+
+'}'+
+'.signature-text{'+
+'  text-align: center;'+
+'}'+
+'.dated-para{'+
+'  margin: 35px 0 56px 0;'+
+'  text-align: center;'+
+'}'+
+'</style>'+
+'  <div class="form-width">'+
+'     <div class="form-heading">'+
+'       <span class="heading-left">Statutory Format of the Instrument of Proxy<br> for Meetings of Corporation</span>'+
+'       <span class="heading-right">Appendix 6</span>'+
+'       '+
+'     </div>'+
+'       <div>'+
+'          <h3 class="heading-instrument">INSTRUMENT OF PROXY FOR MEETINGS OF CORPORATIONS</h3>'+
+'      </div>'+
+'     <div class="form-contain">'+
+'       <p>The Incorporated Owners of <span class="text-color">'+req.user.estateName+'</span>(description of building)</p>'+
+'       <p class="space">I/We,<span class="text-color">'  +req.user.name+'</span>(name(s) of owner(s)), being the owner(s) of <span class="text-color"> '+req.user.unit+'</span>(unit and address of building), hereby appoint <span class="text-color">Telos</span> (name of proxy) *[or failing him <span  class="text-color"></span> (name of alternative proxy)], as my/our proxy to attend and vote on my/our behalf at the *[general meeting/annual general meeting] of The Incorporated Owners of<span>  '+req.user.estateName+'</span>(description of building), to be held on the<span class="text-color"> ' +req.body.endTime+'</span> day of <span class="text-color">'  +req.body.startTime+'</span>*[and at any adjournment therof]</p>'+
+''+
+''+
+'       <p class="dated-para">Dated this day of <span class="text-color">'+ new Date() +'</span> .</p>'+
+'        <p class="signature-text"><span class="text-color">resident.signature</span>(Signature of qwner(s))</p>'+
+'    '+
+'       <span>*Delete where inapplicable.</span>'+
+'     </div>'+
+''+
+'  </div>'+
+'</body>'+
+'</html>'
+pdf.create(html).toBuffer(function(err, buffer){
+  console.log('This is a buffer:', buffer, Buffer.isBuffer(buffer));
+ var data = {
+                Bucket: BucketName,
+                Key: `${req.user.estateName}/ProxyForm/${req.user.name}`,
+                Body: buffer,
+                ContentType: 'application/pdf',
+                ContentDisposition: 'inline',
+                ACL: "public-read"
+            }; // req.user.estateName
+bucket.putObject(data, function (err, data) {
+            if (err) {
+                    console.log('Error uploading data: ', err);
+                } else {
+            console.log('succesfully uploaded the pdf!');
+        }
+  });
+});
+/*var data = convert(html, {format:'png', quality: 100, width: 1280, height: 960})
+console.log(data, "")*/
 
 })
 
